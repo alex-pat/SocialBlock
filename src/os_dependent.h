d0589ea5f8f48bbc09942a71030b3edcccc442df
Alexander Pateenok <pateenoc@gmail.com>
2016-01-21 00:38:52 +0600

    Fixed hosts path in windows

--- src/os_dependent.h
+++ src/os_dependent.h
@@ -4,10 +4,9 @@
 #if defined(Q_OS_LINUX)
 const QString HOSTS = "/etc/hosts";
 #elif defined(Q_OS_WIN)
-const QString HOSTS = "C:\Windows\System32\drivers\etc\hosts";
+const QString HOSTS = "C:\\Windows\\System32\\drivers\etc\\hosts";
 #elif defined(Q_OS_OSX)
 const QString HOSTS = "/private/etc/hosts";
 #endif
 
 #endif // OS_DEPENDENT_H
-
